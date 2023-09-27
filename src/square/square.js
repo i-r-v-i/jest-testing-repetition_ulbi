98d7196ea3f21bce48d50be002885beca72bcf31
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-09-27 23:51:50 +0300

    square function - clearAllMocks

--- src/square/square.js
+++ src/square/square.js
@@ -1,5 +1,8 @@
 const square = (number) => {
-    return number * number;
+    if(number===1) {
+        return 1
+    }
+    return Math.pow(number, 2)
 }
 
 module.exports = square;
--- src/square/square.test.js
+++ src/square/square.test.js
@@ -6,14 +6,30 @@ describe('square', () => {
     beforeEach(() => {
         mockValue = Math.random();
     })
+    // вызывается один раз перед всеми
+    beforeAll(() => {
+
+    })
+    test('Корректное значение', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2);
+        expect(spyMathPow).toBeCalledTimes(1);
+    })
 
     test('Корректное значение', () => {
-    expect(square(2)).toBe(4);
-    expect(square(2)).toBeLessThan(5);
-    expect(square(2)).toBeGreaterThan(3);
-    expect(square(2)).not.toBeUndefined();
+    // expect(square(2)).toBe(4);
+    // expect(square(2)).toBeLessThan(5);
+    // expect(square(2)).toBeGreaterThan(3);
+    // expect(square(2)).not.toBeUndefined();
+    const spyMathPow = jest.spyOn(Math, 'pow');
+    square(1);
+    expect(spyMathPow).toBeCalledTimes(0);
 })
-// test('Мешанина', () => {
-//     expect(squares()).toEqual();
-// })
+
+afterEach(() => {
+jest.clearAllMocks();
+})
+afterAll(() => {
+
+})
 })
